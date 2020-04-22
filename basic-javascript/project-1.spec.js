9e5fd39984527c883b294956cf247d8021a27360
killinq-joke <zakadmaster@gmail.com>
2020-04-22 22:33:23 +0200

    Addition of subtractFive method & tests

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,19 @@
 const helpers = require('./project-1');
 
 // start testing!
+describe("project-1", () => {
+    describe("multiplyByTen()", () => {
+        it("returns the argument multiplied by 10", () => {
+            expect(helpers.multiplyByTen(10)).toBe(100)
+            expect(helpers.multiplyByTen(4)).toBe(40);
+            expect(helpers.multiplyByTen(511)).toBe(5110);
+        })
+    })
+    describe("subtractFive()", () => {
+      it("returns the argument minus 5", () => {
+        expect(helpers.subtractFive(10)).toBe(5);
+        expect(helpers.subtractFive(4)).toBe(-1);
+        expect(helpers.subtractFive(511)).toBe(506);
+      });
+    });
+})
